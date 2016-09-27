490945cfab081d0bf8ec7df1f6ae029f13e9a975
Loric Brevet <loric.brevet@gmail.com>
2016-09-27 22:42:01 +0200

    Refactor everything

---
+++ link.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+import os
+import shutil
+
+def echo(message, depth=0):
+    print('{}> {}'.format('  ' * depth, message))
+
+def rm(path):
+    if os.path.islink(path):
+        os.unlink(path)
+    elif os.path.isdir(path):
+        shutil.rmtree(path)
+    elif os.path.isfile(path):
+        os.remove(path)
+
+def linkAllFiles(src_folder, dest_folder):
+    for file in os.listdir(src_folder):
+        path = os.path.abspath(os.path.join(src_folder, file))
+        link = os.path.join(dest_folder, file)
+        rm(link)
+        echo(file, depth=1)
+        os.symlink(path, link)
+
+HOME = os.environ['HOME']
+
+echo('Linking home')
+linkAllFiles('home', HOME) 
+
+echo('Linking config')
+linkAllFiles('config', os.path.join(HOME, '.config')) 

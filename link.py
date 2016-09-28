e4c6558445fe74ce7d1b396323123ce993f394d6
Loric Brevet <loric.brevet@gmail.com>
2016-09-28 09:33:24 +0200

    Reconfigure sshrc with home tip

--- link.py
+++ link.py
@@ -22,6 +22,11 @@ def linkAllFiles(src_folder, dest_folder):
         echo(file, depth=1)
         os.symlink(path, link)
 
+def linkFile(path, link):
+    rm(link)
+    echo(path, depth=1)
+    os.symlink(os.path.abspath(path), link)
+
 HOME = os.environ['HOME']
 
 echo('Linking home')
@@ -29,3 +34,6 @@ linkAllFiles('home', HOME)
 
 echo('Linking config')
 linkAllFiles('config', os.path.join(HOME, '.config')) 
+
+echo('Linking sshrc.d')
+linkFile('home', os.path.join(HOME, '.sshrc.d'))

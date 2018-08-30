548bb5a7f965910037843da7b77f23ea87256dd0
Loric Brevet <loric.brevet@gmail.com>
2018-08-30 14:56:46 +0200

    Graphical add docker compose

--- graphical.sh
+++ graphical.sh
@@ -146,6 +146,10 @@ sudo apt-get update
 sudo apt-get install -y docker-ce
 sudo usermod -aG docker $USER
 
+# Install docker-comppose
+sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
 # Install snapd
 sudo apt-get install -y snapd
 
